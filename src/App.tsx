d4fcdd5f7393b8ab6ba42aa7c30710fbc895b2b7
Nino Parisi <aparisi@eshopworld.com>
2024-01-17 14:00:28 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { PatientSearch } from './PatientSearch';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="bg-dark text-white text-center py-3">
+        <h1>Your Hospital's Patient Database</h1>
       </header>
+      <main className="container my-5">
+        <PatientSearch />
+      </main>
+      <footer className="bg-dark text-white text-center py-3">
+        &copy; {(new Date()).getFullYear()} Your Hospital Name
+      </footer>
     </div>
   );
 }
